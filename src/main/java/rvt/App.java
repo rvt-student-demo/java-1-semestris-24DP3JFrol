696f4e76555113394946603ecc20cd6d39b1b473
24DP3JFrol <A240132jf@rvt.lv>
2025-10-22 14:39:57 +0300

    Cleaning App file

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,24 +3,7 @@ package rvt;
 import java.util.Scanner;
 
 public class App {
-   public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
         
-        
-        while (true) {
-            System.out.print("Give a number: ");
-            int value = Integer.valueOf(scanner.nextLine());
-            if (value == 0) {
-                break;
-            }
-            if (value < 0) {
-                System.out.println("Unsuitable number");
-            }
-            int rez = value * value;
-            System.out.println(rez);
-        }
-
-
-        scanner.close();
     }
 }
