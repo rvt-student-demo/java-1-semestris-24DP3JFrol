872b4199a886c656bb626413cee18b0c9f736698
24DP3JFrol <A240132jf@rvt.lv>
2025-10-22 14:36:01 +0300

    clearing java unit test class

--- src/test/java/rvt/AppTest.java
+++ src/test/java/rvt/AppTest.java
@@ -3,28 +3,5 @@ package rvt;
 import java.util.Scanner;
 
 public class AppTest {
-    // ...existing code...
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Give a number: ");
-        int first_number = scanner.nextInt();
-
-        System.out.print("Give a second number: ");
-        int second_number = scanner.nextInt();
-
-        int sum = first_number + second_number;
-        int minus = first_number - second_number;
-        int multiply = first_number * second_number;
-        double divide = first_number / second_number;
-
-        System.out.println(first_number + " + " + second_number + " = " + sum);
-        System.out.println(first_number + " - " + second_number + " = " + minus);
-        System.out.println(first_number + " * " + second_number + " = " + multiply);
-        System.out.println(first_number + " / " + second_number + " = " + divide);
-
-
-
-        scanner.close();
-    }
+    
 }
